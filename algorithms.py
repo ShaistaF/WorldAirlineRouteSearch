4b083d71e9c64fc2f732d78e58d074539f8ccf6e
Shaista Firdaws <shaistafirdaws295@gmail.com>
2024-11-21 09:54:16 -0600

    re-arranging files

--- algorithms.py
+++
@@ -1,7 +0,0 @@
-import networkx as nx
-
-def shortest_path(G, origin, destination):
-    try:
-        return nx.dijkstra_path(G, origin, destination)
-    except nx.NetworkXNoPath:
-        return f"No path between {origin} and {destination}"
--- dataprocessing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def load_and_clean_data(file_path):
-    df = pd.read_csv(file_path)
-    df.dropna(inplace=True)
-    df = df[df['distance'] > 0]
-    return df
